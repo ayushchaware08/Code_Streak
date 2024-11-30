94677d0ef385c23fdffbb35d43ab88e210847479
Ayush Chaware <118629631+ayushchaware08@users.noreply.github.com>
2024-11-30 21:39:35 +0530

    Create (D016) 121. Best Time to Buy and Sell Stock (kadane algo).cpp

---
+++ (D016) 121. Best Time to Buy and Sell Stock (kadane algo).cpp
@@ -0,0 +1,22 @@
+// can be optimized by Kadane's Algorithm
+
+// TC = O(n)
+// SC = o(1)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = 0, sell = 1, curr_profit, max_profit = 0;
+        // left ptr (buy) must min and further this right ptr (sell) must be max ele
+        while (sell < prices.size()) {
+            curr_profit = prices[sell] - prices[buy];
+            if (prices[buy] < prices[sell]) {
+                max_profit = max(max_profit, curr_profit);
+            } else {
+                buy = sell;
+            }
+            sell++;
+        }
+        return max_profit;
+    }
+};
