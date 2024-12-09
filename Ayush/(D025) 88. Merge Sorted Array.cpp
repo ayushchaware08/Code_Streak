ddb45ed497b5320ff63a9a1230fd508a396f9377
ayushchaware08 <ayushchaware08@gmail.com>
2024-12-09 23:59:44 +0530

    Day 25

---
+++ Ayush/(D025) 88. Merge Sorted Array.cpp
@@ -0,0 +1,21 @@
+// brute force
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // merge from end
+        int i = m-1, j = n-1, k = m+n-1;
+        // 
+        while((i>=0 && j>=0)){
+            if(nums1[i] >= nums2[j]){ // larger ele at k posn in nums1
+                nums1[k--] = nums1[i--];
+            }
+            else{ 
+                nums1[k--]=nums2[j--];
+            }
+        }
+        // remaning ele in nums2
+        while(j>=0){
+            nums1[k--]=nums2[j--];
+        }
+    }
+};
