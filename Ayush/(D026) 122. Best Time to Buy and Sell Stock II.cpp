c71c23df8acbbd28e2bc00e1a38a7bdc0acd33e7
ayushchaware08 <ayushchaware08@gmail.com>
2024-12-11 00:31:17 +0530

    Day 26

--- Ayush/(D026) 122. Best Time to Buy and Sell Stock II.cpp
+++ Ayush/(D026) 122. Best Time to Buy and Sell Stock II.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int sell=1, curr_p, max_p =0;
-
+        // buy prv day and sell today
         while(sell<prices.size()){
             curr_p = prices[sell] - prices[sell-1];
             if(prices[sell]>prices[sell-1]){ // in profit
---
+++ Ayush/(D026) 55. Jump Game.cpp
@@ -0,0 +1,16 @@
+// tc o(n)
+// sc o(1)
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal =nums.size()-1;
+        for(int i =nums.size()-2; i>=0; i--){
+            // main condition
+            if(nums[i] + i >= goal){
+                goal =i;
+            }
+        }
+        return goal ==0;
+    }
+};
