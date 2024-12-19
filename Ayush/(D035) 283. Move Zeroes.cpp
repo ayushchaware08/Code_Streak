6d87d2c9bafa725a538c9ae69a35a0db4543b418
ayushchaware08 <ayushchaware08@gmail.com>
2024-12-19 23:31:43 +0530

    Day 35

---
+++ Ayush/(D035) 283. Move Zeroes.cpp
@@ -0,0 +1,14 @@
+// TC: O(n)
+// SC: O(1)
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int l=0;
+        for(int r=0; r<nums.size();r++){
+            if(nums[r]!=0){
+                swap(nums[l],nums[r]);
+                l++;
+            }
+        }
+    }
+};
