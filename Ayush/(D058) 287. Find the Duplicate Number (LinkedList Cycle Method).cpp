bd70cf25c1f97d88de03b2034c6e983ec5dbb942
ayushchaware08 <ayushchaware08@gmail.com>
2025-01-11 23:49:00 +0530

    Day 58

---
+++ Ayush/(D058) 287. Find the Duplicate Number (LinkedList Cycle Method).cpp
@@ -0,0 +1,35 @@
+// LinkedList Cycle Method - optimal
+// TC: O(n);
+// SC: O(1);
+
+
+// Hashing approach - Having a frequency array
+// TC: O(n)
+// SC: O(1)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> freq(n,0);
+        for(int i =0; i<n; i++){
+            if(freq[nums[i]] == 0) freq[nums[i]]+=1;
+            else return nums[i];
+        }
+        return 0;
+    }
+};
+
+// Navie Approach Using sorting
+// TC: O(logn + n)
+// SC: O(1)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i =1; i<n; i++){
+            if(nums[i]==nums[i-1]) return nums[i];
+        }
+        return -1;
+    }
+};
