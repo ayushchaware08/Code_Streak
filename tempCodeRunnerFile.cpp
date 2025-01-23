4d18595675131677ba23416ae6c7ed299b1ff3f4
ayushchaware08 <ayushchaware08@gmail.com>
2025-01-23 22:00:08 +0530

    Day 70

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,22 +1,48 @@
-#include<iostream>
-using namspace std;
+#include <iostream>
+using namespace std;
 
-int rotateImage(int matrix[3][3], int n){
-    int ans[3][3];
-    for(int i =0; i<n; i++){
-        for(int j = 0; j<n; j++){
-            ans[j][n-1-i]=matrix[i][j];
-        }
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+	   int i = low - 1;
+	   
+        for (int j = low; j < high; ++j){
+        	if (arr[j] <= pivot) {
+		   swap(arr[++i], arr[j]);
+		}	  
+	   }
+		  
+        swap(arr[i + 1], arr[high]);
+        int pi = i + 1;
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
-    return ans;
 }
 
-int main(){ 
-    int matrix[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
-    rotateImage(matrix, 3);
-    for(int i =0; i<3; i++){
-        for(int j=0; j<3; j++){
-            cout<<matrix[i][j]<<" ";
-        }
+int main() {
+    
+    cout<<"Name : Ayush Chaware\n";
+    cout<<"Roll No.  22\n";
+    cout<<"Branch : AI\n";
+    
+     int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
+    
+    int n1 = sizeof(arr) / sizeof(arr[0]);
+    
+    quickSort(arr, 0, n1 - 1);
+    
+    for (int i = 0; i < n1; ++i) {
+    cout << arr[i] << " ";
+    }
+    return 0;
 }
